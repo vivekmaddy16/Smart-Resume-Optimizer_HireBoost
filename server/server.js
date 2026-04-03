67ffd03a602210de0a5807e15ff923ae6bc740cc
Vivek Maddheshiya <vivekmaddheshiya19@gmail.com>
2026-04-03 17:45:14 +0530

    Setup: Configure environment variables for Render + Vercel deployment

--- server/server.js
+++ server/server.js
@@ -12,8 +12,10 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://localhost:3000',
-  'http://127.0.0.1:3000'
-];
+  'http://127.0.0.1:3000',
+  process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : null,
+  'https://hireboost.vercel.app'
+].filter(Boolean);
 
 // Middleware
 app.use(cors({
